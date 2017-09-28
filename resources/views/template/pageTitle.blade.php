acb27d81bc442af39658d505a2742a91fa440473
d33cktr4zy <gabriel.aswinta@gmail.com>
2017-09-28 08:18:05 +0700

    Navigation added

---
+++ resources/views/template/pageTitle.blade.php
@@ -0,0 +1,6 @@
+<div class="row" id="pageTitle">
+    <div class="col-md-12">
+        <h4 id="titlePlaceHolder" class="text-right"><hr />@yield('pageTitle')</h4>
+        <hr />
+    </div>
+</div>
---
+++ resources/views/template/title.blade.php
@@ -0,0 +1,5 @@
+<div class="row" data-pg-collapsed>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <h3 style="display: inline-block;" class="show"><h1 style="display: block;">HKBP Menteng<br></h1>Aplikasi Pendataan Umat Gereja</h3>
+    </div>
+</div>
